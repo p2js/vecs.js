2fb193ef7d57b382bd0dc9c9010d7a33ef33f025
Alfio <tomarchioalfio5@gmail.com>
2022-09-01 21:20:49 +0200

    Update vecs.js
    This update to vecs.js adds a couple of new features:
    - support for the spread operator in vectors so that they can be used in the definition for other vectors as well as other functions that use a rest paramterer
    - the multidimensional getters now output other vectors instead of arrays
    - the proxies and argument processors have been consolidated into 1
    - support for vector construction with no parameters (will make a vector with 0 as all of its values), or 1 parameter (will make a vector with that parameter as all of its values)
    - proper error handling and argument processing in the construction of the vectors

--- vecs.js
+++ vecs.js
@@ -1,138 +1,79 @@
-const vec2 = (a, b) => {
-    const obj = {
-        x: a,
-        y: b,
-    };
-
-    const processProxyArgument = (arg) => {
-        arg = arg.toString();
-            switch (arg) {
-                case "0":
-                case "r":
-                case "s":
-                    arg = "x";
-                    break;
-                case "1":
-                case "g":
-                case "t":
-                    arg = "y";
-                    break;
-        }
-        return arg;
+const processProxyArgument = (arg) => {
+    arg = arg.toString();
+    switch (arg) {
+        case "0":
+        case "r":
+        case "s":
+            arg = "x";
+            break;
+        case "1":
+        case "g":
+        case "t":
+            arg = "y";
+            break;
+        case "2":
+        case "b":
+        case "p":
+            arg = "z";
+            break;
+        case "3":
+        case "a":
+        case "q":
+            arg = "w";
+            break;
     }
+    return arg;
+}
 
-    const proxyHandler = {
-        get: function (obj, prop) {
-            if (prop.length == 1) { 
-                return obj[processProxyArgument(prop)];
-            } else {
-                return prop.substring(0, 4).split("").map(p => obj[processProxyArgument(p)]);
-            }       
-        },
-        set: function (obj, prop, value) {
-            obj[processProxyArgument(prop)] = value;
+const proxyHandler = {
+    get: function (obj, prop) {
+        if (prop === Symbol.iterator) {
+            return function* () {
+                for(const key of Object.keys(obj)) yield obj[key];
+            }
         }
+        if (prop.length == 1) {
+            return obj[processProxyArgument(prop)];
+        } else {
+            prop = prop.substring(0, 4).split("").map(p => obj[processProxyArgument(p)]);
+            return eval(`vec${prop.length}`)(...prop);
+        }
+    },
+    set: function (obj, prop, value) {
+        obj[processProxyArgument(prop)] = value;
     }
+};
 
-    let out = new Proxy(obj, proxyHandler);
+const vec2 = (x, y) => {
+    if(x === undefined) return vec2(0, 0);
+    if(y === undefined) return vec2(x, x);
+    x = Number(x);
+    y = Number(y);
+    let out = new Proxy({ x, y }, proxyHandler);
     return out;
 };
 
-const vec3 = (a, b, c) => {
-    const obj = {
-        x: a,
-        y: b,
-        z: c
-    };
-
-    const processProxyArgument = (arg) => {
-        arg = arg.toString();
-            switch (arg) {
-                case "0":
-                case "r":
-                case "s":
-                    arg = "x";
-                    break;
-                case "1":
-                case "g":
-                case "t":
-                    arg = "y";
-                    break;
-                case "2":
-                case "b":
-                case "p":
-                    arg = "z";
-                    break;
-        }
-        return arg;
-    }
-
-    const proxyHandler = {
-        get: function (obj, prop) {
-            if (prop.length == 1) { 
-                return obj[processProxyArgument(prop)];
-            } else {
-                return prop.substring(0, 4).split("").map(p => obj[processProxyArgument(p)]);
-            }            
-        },
-        set: function (obj, prop, value) {
-            obj[processProxyArgument(prop)] = value;
-        }
-    }
-
-    let out = new Proxy(obj, proxyHandler);
+const vec3 = (x, y, z) => {
+    if(x === undefined) return vec3(0, 0, 0);
+    if(y === undefined) return vec3(x, x, x);
+    if(z === undefined) throw error("Not enough data provided for construction");
+    x = Number(x);
+    y = Number(y);
+    z = Number(z);
+    let out = new Proxy({ x, y, z }, proxyHandler);
     return out;
 }
 
-const vec4 = (a, b, c, d) => {
-    const obj = {
-        x: a,
-        y: b,
-        z: c,
-        w: d
-    };
-
-    const processProxyArgument = (arg) => {
-        arg = arg.toString();
-            switch (arg) {
-                case "0":
-                case "r":
-                case "s":
-                    arg = "x";
-                    break;
-                case "1":
-                case "g":
-                case "t":
-                    arg = "y";
-                    break;
-                case "2":
-                case "b":
-                case "p":
-                    arg = "z";
-                    break;
-                case "3":
-                case "a":
-                case "q":
-                    arg = "w";
-                    break;
-        }
-        return arg;
-    }
-
-    const proxyHandler = {
-        get: function (obj, prop) {
-            if (prop.length == 1) { 
-                return obj[processProxyArgument(prop)];
-            } else {
-                return prop.substring(0, 4).split("").map(p => obj[processProxyArgument(p)]);
-            }            
-        },
-        set: function (obj, prop, value) {
-            obj[processProxyArgument(prop)] = value;
-        }
-    }
-
-    let out = new Proxy(obj, proxyHandler);
+const vec4 = (x, y, z, w) => {
+    if (x === undefined) return vec3(0, 0, 0);
+    if (y === undefined) return vec3(x, x, x);
+    if (z === undefined) throw error("Not enough data provided for construction");
+    if (w === undefined) throw error("Not enough data provided for construction");
+    x = Number(x);
+    y = Number(y);
+    z = Number(z);
+    w = Number(w);
+    let out = new Proxy({ x, y, z, w }, proxyHandler);
     return out;
 }
 
