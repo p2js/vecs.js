842e498e7a86e9d5774907700f7fe7ddd7027b97
Alfio <tomarchioalfio5@gmail.com>
2022-09-13 12:52:22 +0200

    Update vecs.js
    This update adds support for the `new` keyboard when defining a vector, support for the `instanceof` operator and a property to access the constructor, refactors the code a little bit, and switches to CommonJS modules.

--- vecs.js
+++ vecs.js
@@ -1,4 +1,4 @@
-const processProxyArgument = (arg) => {
+const processProxyArgument = arg => {
     arg = arg.toString();
     switch (arg) {
         case "0":
@@ -23,57 +23,65 @@ const processProxyArgument = (arg) => {
             break;
     }
     return arg;
-}
+};
 
 const proxyHandler = {
     get: function (obj, prop) {
         if (prop === Symbol.iterator) {
             return function* () {
-                for(const key of Object.keys(obj)) yield obj[key];
+                for (const key of Object.keys(obj)) yield obj[key];
             }
-        }
+        };
         if (prop.length == 1) {
             return obj[processProxyArgument(prop)];
         } else {
+            let s = Object.keys(obj).length;
+            if (prop === "constructor") return eval(`vec${s}`);
             prop = prop.substring(0, 4).split("").map(p => obj[processProxyArgument(p)]);
             return eval(`vec${prop.length}`)(...prop);
-        }
+        };
     },
-    set: function (obj, prop, value) {
-        obj[processProxyArgument(prop)] = value;
+    set: function (obj, _prop, _value) {
+        obj[processProxyArgument(_prop)] = _value;
+        return true;
     }
 };
 
-const vec2 = (x, y) => {
-    if(x === undefined) return vec2(0, 0);
-    if(y === undefined) return vec2(x, x);
-    x = Number(x);
-    y = Number(y);
-    let out = new Proxy({ x, y }, proxyHandler);
-    return out;
+function vec2(x, y) {
+    if (x === undefined) return vec2(0, 0);
+    if (y === undefined) return vec2(x, x);
+    [x, y] = [x, y].map(Number);
+    let obj = {x, y}
+    Object.seal(obj);
+    return new Proxy(obj, proxyHandler);
 };
 
-const vec3 = (x, y, z) => {
-    if(x === undefined) return vec3(0, 0, 0);
-    if(y === undefined) return vec3(x, x, x);
-    if(z === undefined) throw error("Not enough data provided for construction");
-    x = Number(x);
-    y = Number(y);
-    z = Number(z);
-    let out = new Proxy({ x, y, z }, proxyHandler);
-    return out;
-}
+function vec3(x, y, z) {
+    if (x === undefined) return vec3(0, 0, 0);
+    if (y === undefined) return vec3(x, x, x);
+    if (z === undefined) throw error("Not enough data provided for construction");
+    [x, y, z] = [x, y, z].map(Number);
+    let obj = { x, y, z };
+    Object.seal(obj);
+    return new Proxy(obj, proxyHandler);
+};
 
-const vec4 = (x, y, z, w) => {
+function vec4(x, y, z, w) {
     if (x === undefined) return vec4(0, 0, 0, 0);
     if (y === undefined) return vec4(x, x, x, x);
     if (z === undefined || w === undefined) throw error("Not enough data provided for construction");
-    x = Number(x);
-    y = Number(y);
-    z = Number(z);
-    w = Number(w);
-    let out = new Proxy({ x, y, z, w }, proxyHandler);
-    return out;
-}
+    [x, y, z, w] = [x, y, z, w].map(Number);
+    let obj = { x, y, z, w };
+    Object.seal(obj);
+    return new Proxy(obj, proxyHandler);
+};
 
-export default { vec2, vec3, vec4 };
+//instanceof support
+[vec2, vec3, vec4].forEach(v => Object.defineProperty(v, Symbol.hasInstance, {
+    value: function(i) {
+        if(i.constructor == v) return true;
+        else return false;
+    }
+}));
+
+module.exports = { vec2, vec3, vec4 };
